d7f24e6eae6b47fc425d771693b5abe2f827b1ba
Karol Borecki <karol.borecki@onet.eu>
2019-10-05 13:12:37 +0200

    Error in code has been repaird

--- ld45/Assets/Scripts/Singing/SingingTrialGenerator.cs
+++ ld45/Assets/Scripts/Singing/SingingTrialGenerator.cs
@@ -4,26 +4,5 @@ using UnityEngine;
 
 public class SingingTrialGenerator : MonoBehaviour
 {
-	public Vector2 pointsCountMaxs;
-	public List<Vector2> trialPoints;
-
-	private int _pointsCount;
-
-    void Start()
-    {
-		pointsCount = Random.Range(0, pointsCountMaxs - 1);
-
-    }
-
-    void Update()
-    {
-        
-    }
-
-    private void CreateRandomPoints()
-	{
-        foreach(int i in pointsCount)
-		{
-		}
-	}
+	
 }
