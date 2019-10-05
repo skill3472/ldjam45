26ad4b3e9bf0be2340973ff9ddae75948ec1600d
Karol Borecki <karol.borecki@onet.eu>
2019-10-05 13:09:45 +0200

    Added singing game

---
+++ ld45/Assets/Scripts/Singing/SingingTrialGenerator.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SingingTrialGenerator : MonoBehaviour
+{
+	public Vector2 pointsCountMaxs;
+	public List<Vector2> trialPoints;
+
+	private int _pointsCount;
+
+    void Start()
+    {
+		pointsCount = Random.Range(0, pointsCountMaxs - 1);
+
+    }
+
+    void Update()
+    {
+        
+    }
+
+    private void CreateRandomPoints()
+	{
+        foreach(int i in pointsCount)
+		{
+		}
+	}
+}
