67d6c91a8dde2fb1406a94f37ba9fbb01d3979bc
Maksymilian Tym <tymszczur@gmail.com>
2019-10-05 11:28:41 +0200

    Made a working prototype

---
+++ ld45/Assets/Scripts/meditating.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class meditating : MonoBehaviour
+{
+
+	public Transform[] spawnpoints;
+	public float timeBeforeNextSpawn;
+	public GameObject thought;
+	public float LocalDifficulty;
+	public int points;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+    	points = 0;
+        timeBeforeNextSpawn = 5f;
+        LocalDifficulty = 1f;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        timeBeforeNextSpawn -= Time.deltaTime;
+        if(timeBeforeNextSpawn <= 0)
+        {
+        	LocalDifficulty += 0.1f;
+        	timeBeforeNextSpawn = 2 / LocalDifficulty;
+        	SpawnThought();
+        }
+    }
+
+    public void SpawnThought()
+    {
+    	Instantiate(thought, spawnpoints[(int) Mathf.Round(Random.Range(0,6))].position, transform.rotation);
+    }
+}
---
+++ ld45/Assets/Scripts/thoughtCtrl.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class thoughtCtrl : MonoBehaviour
+{
+	private GameObject gm;
+	[SerializeField]
+	private Text thoughtText;
+	public string[] possibleThoughts;
+	[SerializeField]
+	private bool isLeft;
+	//public float movementTick;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+    	thoughtText = this.gameObject.transform.GetChild(0).GetComponent<Text>();
+    	thoughtText.text = possibleThoughts[(int) Mathf.Round(Random.Range(0,5))];
+    	gm = GameObject.Find("_GM");
+        //Checking which way the thought should be heading
+        if(transform.position.x > 0)
+        {
+        	isLeft = false;
+        }
+        else
+        {
+        	isLeft = true;
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+    	 //Move Right
+    	 if(isLeft)
+    	 {
+    	 	transform.Translate(transform.right * Time.deltaTime * 2);
+    	 }
+    	 //Move Left
+    	 else
+    	 {
+			transform.Translate(-transform.right * Time.deltaTime * 2);	
+    	 }
+    	 
+    }
+
+    void OnMouseDown()
+    {
+    	gm.GetComponent<meditating>().points++;
+    	GameObject.Destroy(gameObject);
+    }
+}
