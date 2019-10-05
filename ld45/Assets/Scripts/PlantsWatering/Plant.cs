a56f313be4cfbd37cddc2d3e9a37370cdabcadee
Karol Borecki <karol.borecki@onet.eu>
2019-10-05 10:49:12 +0200

    PlantsWatering has been added
    +plants Spawn
    +plants initial values

---
+++ ld45/Assets/Scripts/PlantsWatering/Plant.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(SpriteRenderer))]
+public class Plant : MonoBehaviour
+{
+    public bool state = true;
+    public int waterLevel = 1;
+
+    private SpriteRenderer _spiteRenderer;
+
+    private void Start()
+    {
+        _spiteRenderer = gameObject.GetComponent<SpriteRenderer>();
+    }
+
+    private void Update()
+    {
+        
+    }
+
+    private void AddWater()
+    {
+        waterLevel++;
+        //UpdateSprite
+        _spiteRenderer.color = new Color(_spiteRenderer.color.r + 20, _spiteRenderer.color.g + 20, _spiteRenderer.color.b + 20);
+    }
+
+    private void OnMouseDown()
+    {
+        AddWater();
+    }
+}
---
+++ ld45/Assets/Scripts/PlantsWatering/PlantRowManager.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlantRowManager : MonoBehaviour
+{
+    public List<Transform> plantsPoints;
+    public List<Plant> plantsTypes;
+
+    private void Start()
+    {
+        SpawnPlants();
+    }
+
+    private void Update()
+    {
+        
+    }
+
+    private void SpawnPlants()
+    {
+        foreach(Transform t in plantsPoints)
+        {
+            Instantiate(plantsTypes[Random.Range(0, plantsTypes.Count-1)], t);
+        }
+    }
+}
