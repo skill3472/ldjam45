09b123bf48283ffc6cc8421028e98d295449c2db
Puszekkot <kubalacki1@gmail.com>
2019-10-06 17:41:02 +0200

    Prawie skończyłem!!!!!

---
+++ ld45/Assets/Scripts/Rocket/Asteroid.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Asteroid : MonoBehaviour
+{
+    public GameObject explosion;
+    void Death()
+    {
+        GameObject newExp=Instantiate(explosion);
+        newExp.transform.position=transform.position;
+        Destroy(newExp,2);
+        Destroy(this.gameObject);
+    }
+    private void OnCollisionEnter2D(Collision2D other) {
+        if(other.transform.tag=="bullet")
+        {
+            Death();
+        }    
+    }
+}
