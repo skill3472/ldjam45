7bd8a5daa5e03e8670d519656b9376870109961a
Puszekkot <kubalacki1@gmail.com>
2019-10-06 17:01:48 +0200

    rakieta12

--- ld45/Assets/Scripts/Rocket/RocketController.cs
+++ ld45/Assets/Scripts/Rocket/RocketController.cs
@@ -4,12 +4,17 @@ using UnityEngine;
 
 public class RocketController : MonoBehaviour
 {
+    Animator animator;
+    [SerializeField]
+    private GameObject laser;
     private Rigidbody2D rb2d;
     [SerializeField]
-    private float speed,hp;
+    private float speed,hp,ammo;
+    bool canShoot=true;
     // Start is called before the first frame update
     void Start()
     {
+        animator=GetComponent<Animator>();
         rb2d=GetComponent<Rigidbody2D>();
     }
 
@@ -18,5 +23,22 @@ public class RocketController : MonoBehaviour
     {
         Vector2 targetPosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
         transform.position = Vector3.MoveTowards(transform.position, targetPosition, Time.deltaTime * speed);
+        if(ammo>0&&canShoot)
+        {
+            StartCoroutine(Shoot());
+            canShoot=false;
+        }
+    }
+    IEnumerator Shoot()
+    {
+        yield return new WaitForSeconds(1);
+        ammo--;
+        GameObject newLaser=Instantiate(laser);
+        newLaser.GetComponent<Rigidbody2D>().AddForce(new Vector2(0,12));
+        newLaser.GetComponent<Rigidbody2D>().gravityScale=-8;
+        newLaser.transform.position=transform.position;
+        Destroy(newLaser,2);
+        if(ammo>0)
+            StartCoroutine(Shoot());
     }
 }
