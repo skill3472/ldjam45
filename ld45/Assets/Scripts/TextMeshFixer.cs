f72590e187c3a98de49b2e55ae6b346ad3aff37c
Karol Borecki <karol.borecki@onet.eu>
2019-10-06 13:38:02 +0200

    Added Points Effect

---
+++ ld45/Assets/Scripts/TextMeshFixer.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(TextMesh))]
+public class TextMeshFixer : MonoBehaviour
+{
+    void Start()
+    {
+        GetComponent<MeshRenderer>().sortingLayerName = "UI";
+        GetComponent<MeshRenderer>().sortingOrder = 100;
+
+    }
+
+}
