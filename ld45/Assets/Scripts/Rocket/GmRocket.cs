6c7592c6f108e867d299223afb3ac7e54e0893d7
Puszekkot <kubalacki1@gmail.com>
2019-10-06 14:21:05 +0200

    rakietatest-3

--- ld45/Assets/Scripts/Rocket/GmRocket.cs
+++ ld45/Assets/Scripts/Rocket/GmRocket.cs
@@ -4,17 +4,25 @@ using UnityEngine;
 
 public class GmRocket : MonoBehaviour
 {
+    public static GmRocket instance;
     public float speed;
     private GameObject background;
+    private Rigidbody2D rb2dOfBack;
+    private void Awake() {
+        if(instance!=null)
+            instance=this;    
+    }
     // Start is called before the first frame update
     void Start()
     {
         background=GameObject.Find("tlorakieta");
+        rb2dOfBack=background.GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        background.GetComponent<Rigidbody2D>().AddForce(new Vector2(0,speed));
+        if(rb2dOfBack.velocity.magnitude<-speed)
+            rb2dOfBack.AddForce(new Vector2(0,speed));
     }
 }
