0e338fbf26ef05243270887dd2c2d08a2697c83b
Puszekkot <kubalacki1@gmail.com>
2019-10-06 15:07:44 +0200

    Rakieta6

---
+++ ld45/Assets/Scripts/Rocket/Spawner.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spawner : MonoBehaviour
+{
+    public List<GameObject> spawnPoints;
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartCoroutine(spawnThings());
+    }
+    IEnumerator spawnThings()
+    {
+        yield return new WaitForSeconds(3);
+        spawnThings();
+    }
+}
