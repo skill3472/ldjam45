53105a2305b7937a74e62382875dfb34c559b3b1
Puszekkot <kubalacki1@gmail.com>
2019-10-06 16:05:37 +0200

    rakieta10

--- ld45/Assets/Scripts/Rocket/Spawner.cs
+++ ld45/Assets/Scripts/Rocket/Spawner.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class Spawner : MonoBehaviour
 {
-    public List<GameObject> spawnPoints;
+    
     public List<GameObject> objectsToSpawn;
     // Start is called before the first frame update
     void Start()
@@ -13,8 +13,12 @@ public class Spawner : MonoBehaviour
     }
     IEnumerator spawnThings()
     {
+        transform.position=new Vector2(Random.Range(-10,10),transform.position.y);
         GameObject newObj=Instantiate(objectsToSpawn[Random.Range(0,objectsToSpawn.Count-1)]);
-        yield return new WaitForSeconds(3);
+        newObj.transform.position=transform.position;
+        newObj.transform.GetComponent<Rigidbody2D>().gravityScale=Random.Range(0.4f,2);
+        
+        yield return new WaitForSeconds(1);
         StartCoroutine(spawnThings());
     }
 }
