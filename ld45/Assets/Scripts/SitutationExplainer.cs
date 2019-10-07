feca94c337e0b862ab0e298d6c947d59ca0b5cd3
Karol Borecki <karol.borecki@onet.eu>
2019-10-07 18:00:32 +0200

    Added Presplash Scene - situation explainer

---
+++ ld45/Assets/Scripts/SitutationExplainer.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SitutationExplainer : MonoBehaviour
+{
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+            GoNext();
+    }
+
+    private void GoNext()
+    {
+        Camera.main.GetComponent<CameaBahaviourHandler>().FadeOut();
+    }
+}
